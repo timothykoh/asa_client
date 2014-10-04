89ca25fcf2116f6d252e3865384a1b90132a93ef
Timothy Koh <k.o.htimothy@gmail.com>
2014-10-04 13:59:47 -0400

    added pull to refresh for event list

--- www/js/controllers/events/event_list.js
+++ www/js/controllers/events/event_list.js
@@ -1,28 +1,42 @@
 app.controller("EventListCtrl",
     ["$scope", "EventService", "AuthService", "$state", "$ionicLoading",
     function($scope, EventService, AuthService, $state, $ionicLoading){
+        function updateEventList(){
+            var eventPromise = EventService.getEvents();
+            var userPromise = AuthService.getUser();
+
+            return Promise.all([eventPromise, userPromise]).then(function(values){
+                $scope.$apply(function(){
+                    $scope.events = values[0];
+                    var userObj = values[1];
+                    if (userObj !== undefined){
+                        $scope.isLoggedIn = true;
+                        $scope.isAdmin = userObj.is_admin;
+                    }
+                });
+            });
+        }
+
         $ionicLoading.show({
             template: "<i class='loading-icon ion-loading-a'></i>",
             delay: 300
         });
-        EventService.getEvents().then(function(events){
-            $scope.$apply(function(){
-                $scope.events = events;
-                $ionicLoading.hide();
-            });
+
+        updateEventList().then(function(){
+            $ionicLoading.hide();
         });
 
-        AuthService.getUser().then(function(userObj){
-            $scope.$apply(function(){
-                if (userObj !== undefined){
-                    $scope.isLoggedIn = true;
-                    $scope.isAdmin = userObj.is_admin;
-                }    
-            });
-        });
-
+        
         $scope.viewEvent = function(idx){
             EventService.updateCurrentEvent($scope.events[idx]);
             $state.go("event");
+        };
+
+        $scope.refresh = function(){
+            updateEventList().then(function(){
+                $scope.$broadcast('scroll.refreshComplete');
+            }, function(){
+                $scope.$broadcast('scroll.refreshComplete');
+            });
         };
 }]);
