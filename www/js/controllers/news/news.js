6f1b8b530a8b848657dfd6fa63548ae07f0e73f1
Timothy Koh <k.o.htimothy@gmail.com>
2014-10-04 14:20:16 -0400

    added pull to refresh for news

--- www/js/controllers/news/news.js
+++ www/js/controllers/news/news.js
@@ -1,24 +1,37 @@
 app.controller("NewsCtrl",
     ["$scope", "NewsService", "AuthService", "$ionicLoading",
     function($scope, NewsService, AuthService, $ionicLoading){
-        AuthService.getUser().then(function(userObj){
-            $scope.$apply(function(){
-                if (userObj !== undefined){
-                    $scope.isLoggedIn = true;
-                }
+        function updateNews(){
+            var userPromise = AuthService.getUser();
+            var newsPromise = NewsService.getNews();
+
+            return Promise.all([userPromise, newsPromise]).then(function(values){
+                $scope.$apply(function(){
+                    var userObj = values[0];
+                    if (userObj !== undefined){
+                        $scope.isLoggedIn = true;
+                    }
+                    $scope.newsList = values[1];
+                });
             });
-        });
+            
+        }
 
         $ionicLoading.show({
             template: "<i class='loading-icon ion-loading-a'></i>",
             delay: 300
         });
-        NewsService.getNews().then(function(newsObjArr){
-            console.log(newsObjArr);
-            $scope.$apply(function(){
-                $scope.newsList = newsObjArr;
-                $ionicLoading.hide();
-            });
+
+        updateNews().then(function(){
+            $ionicLoading.hide();
         });
+
+        $scope.refresh = function(){
+            updateNews().then(function(){
+                $scope.$broadcast('scroll.refreshComplete');
+            }, function(){
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
     }]
 );
